45997c26271238d3e1bc4ff4d0db505c303be196
SzymonKrysztofiak <s.krysztofiak@outlook.com>
2021-05-08 20:45:43 +0200

    Add fibonacci route and view

---
+++ routes/fibonacci.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const math = require('../math')
+
+router.get('/', function(req, res, next) {
+  if (req.query.fibonacciNumber) {
+    res.render('fibonacci', {
+      title: 'Calculate Fibonacci numbers',
+      fibonacciNumber: req.query.fibonacciNumber,
+      fibonacciValue: math.fibonacci(req.query.fibonacciValue),
+    })
+  } else {
+    res.render('fibonacci', {
+      title: 'Calculate Fibonacci numbers',
+      fibonacciNumber: undefined,
+    })
+  }
+})
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fibonacci Test' });
-});
+  res.render('index', { title: 'Fibonacci Calculator' })
+})
 
-module.exports = router;
+module.exports = router
