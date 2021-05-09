e05886b800aca6cec9c4887b3a44dbb80a053a04
SzymonKrysztofiak <s.krysztofiak@outlook.com>
2021-05-09 11:05:10 +0200

    Fibonacci log script

---
+++ fibotimes.js
@@ -0,0 +1,6 @@
+const math = require('./math');
+
+for(let number = 1; number < 80; number++) {
+  let now = new Date().toISOString()
+  console.log(`${now} Fibonacci for ${number} = ${math.fibonacci(number)}`)
+}
